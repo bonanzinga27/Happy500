1fc84ae459509cbc4cfed42eb613e7769dbdd42e
Leonardo Galati <leonardogalati91@gmail.com>
2017-02-24 18:32:11 +0100

    Added /getFinanziamento API

--- ebuonweekend/ebuonweekend-web/src/main/java/it/uiip/digitalgarage/ebuonweekend/idao/PraticaDAO.java
+++ ebuonweekend/ebuonweekend-web/src/main/java/it/uiip/digitalgarage/ebuonweekend/idao/PraticaDAO.java
@@ -1,9 +1,12 @@
 package it.uiip.digitalgarage.ebuonweekend.idao;
 
 import it.uiip.digitalgarage.ebuonweekend.entity.Pratica;
+import it.uiip.digitalgarage.ebuonweekend.entity.TipoFinanziamento;
 
 public interface PraticaDAO{
 
     boolean insert(Pratica p);
+
+    TipoFinanziamento[] getAllTipoFinanziamento();
 
 }
