d47c5ba826f78beaf1f2ccab06effb79d4db883f
Leonardo Galati <leonardogalati91@gmail.com>
2017-02-24 18:16:17 +0100

    Added TipoFinanziamento entity

---
+++ ebuonweekend/ebuonweekend-web/src/main/java/it/uiip/digitalgarage/ebuonweekend/entity/TipoFinanziamento.java
@@ -0,0 +1,38 @@
+package it.uiip.digitalgarage.ebuonweekend.entity;
+
+public class TipoFinanziamento {
+
+    private int id;
+    private String nome;
+    private String descrizione;
+
+    public TipoFinanziamento(int id, String nome, String descrizione) {
+        this.id = id;
+        this.nome = nome;
+        this.descrizione = descrizione;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+}
