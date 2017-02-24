0fc848758311fd39492d4c9b0fe6706ba8ecc7ae
ciccarellipasquale <ciccarellipa@gmail.com>
2017-02-24 16:48:07 +0100

    register service WIP

---
+++ ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/register.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {Http, Response} from "@angular/http";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class RegisterService {
+
+  constructor(
+    private http: Http
+  ) { }
+
+  signup(user){
+    return this.http.get('http://localhost:8080/register?email='+user.email+'&pass='+user.password)
+      .map((res:Response) => res.json());
+  }
+
+
+}
