595978677d752b155e1af61019f25dfc8a61ebc9
Lorenzo Gagliani <lorenz_g@hotmail.it>
2017-02-25 02:54:24 +0100

    kmkmk

--- ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/app.module.ts
+++ ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/app.module.ts
@@ -12,16 +12,18 @@ import { SignupComponent } from './signup/signup.component';
 import { FormPraticheComponent } from './form-pratiche/form-pratiche.component';
 import { CardContainerComponent } from './card-container/card-container.component';
 import { CardComponent } from './card/card.component';
-
+import { DatiPraticaComponent } from './dati-pratica/dati-pratica.component';
+import { DocumentazioneComponent } from './documentazione/documentazione.component';
 import { ItemListComponent } from './item-list/item-list.component';
+import { DatiPersonaliComponent } from './dati-personali/dati-personali.component';
+import { DatiOrganizzazioneComponent } from './dati-organizzazione/dati-organizzazione.component';
+
 import {TabsService} from "./tabs.service";
 import {TypesFundingService} from "./types-funding.service";
 import {RegisterService} from "./register.service";
-import { DatiPersonaliComponent } from './dati-personali/dati-personali.component';
-import { DatiOrganizzazioneComponent } from './dati-organizzazione/dati-organizzazione.component';
 import {LoginService} from "./login.service";
-import { DatiPraticaComponent } from './dati-pratica/dati-pratica.component';
-import { DocumentazioneComponent } from './documentazione/documentazione.component';
+import {PraticaService} from "./pratica.service";
+
 
 
 const appRoutes: Routes = [
@@ -59,7 +61,8 @@ const appRoutes: Routes = [
     TabsService,
     RegisterService,
     LoginService,
-    Cookie
+    Cookie,
+    PraticaService
   ],
   bootstrap: [AppComponent]
 })
--- ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/dati-personali/dati-personali.component.ts
+++ ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/dati-personali/dati-personali.component.ts
@@ -9,8 +9,22 @@ import {FormGroup, Validators, FormControl} from "@angular/forms";
 export class DatiPersonaliComponent implements OnInit {
 
   public datiPersonaliForm = new FormGroup({
-    email: new FormControl("", Validators.required),
-    password: new FormControl("", Validators.required)
+    nome:             new FormControl("", Validators.required),
+    cognome:          new FormControl("", Validators.required),
+    codFisc:          new FormControl("", Validators.required),
+    dataNascita:      new FormControl("", Validators.required),
+    cittaNascita:     new FormControl("", Validators.required),
+    provNascita:      new FormControl("", Validators.required),
+    telefono:         new FormControl("", Validators.required),
+    cittaResidenza:   new FormControl("", Validators.required),
+    provResidenza:    new FormControl("", Validators.required),
+    indirizzoResidenza:new FormControl("", Validators.required),
+    emailUtente:      new FormControl("", Validators.required),
+    emailRichiedente: new FormControl("", Validators.required),
+    cartaIdentitaPath:new FormControl("", Validators.required),
+    codiceFiscPath:   new FormControl("", Validators.required),
+    sesso:            new FormControl("", Validators.required)
+
   });
 
   constructor() { }
