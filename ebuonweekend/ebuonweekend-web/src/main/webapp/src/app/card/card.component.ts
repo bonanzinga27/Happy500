28ccb2a6e4765e66e0958267d44498f10a069a07
Lorenzo Gagliani <lorenz_g@hotmail.it>
2017-02-25 05:02:11 +0100

    PULLATEVI TUTTI

--- ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/card/card.component.ts
+++ ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/card/card.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {TypesFundingService} from "../types-funding.service";
+import {Cookie} from "ng2-cookies";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-card',
@@ -9,9 +12,23 @@ export class CardComponent implements OnInit {
 
   @Input() servizio;
 
-  constructor() { }
+  constructor(
+    private fundingService: TypesFundingService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  nuovaPratica(nomeFinanziamento){
+    this.fundingService.nuovaPratica(nomeFinanziamento, Cookie.get('email'))
+      .subscribe(risposta => {
+        if (risposta.returnObject != null){
+          this.router.navigate(['../pratica']);
+        }else{
+          console.log("errore, nella creazione della nuova pratica");
+        }
+      });
   }
 
 }
--- ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/types-funding.service.ts
+++ ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/types-funding.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {Observable} from "rxjs";
 
 
 @Injectable()
@@ -13,6 +14,23 @@ export class TypesFundingService {
   getList() {
     return this.http.get('http://localhost:8080/getFinanziamenti')
       .map((res:Response) => res.json());
+  }
+
+  nuovaPratica(nomeFinanz, myEmail) {
+    let api = 'http://localhost:8080/insertPratica?email='+myEmail+'&tipo='+nomeFinanz;
+
+    let tmp = [
+      { email: myEmail },
+      { tipo: nomeFinanz }
+    ];
+
+    let bodyString = JSON.stringify(tmp); // Stringify payload
+    let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+    let options = new RequestOptions({ headers: headers }); // Create a request option
+
+    return this.http.post(api, bodyString, options) // ...using post request
+      .map((res: Response) => res.json()) // ...and calling .json() on the response to return data
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
   }
 
 }
