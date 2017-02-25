70b1612422a73437fb17bbaa430975c66a88bcf0
Lorenzo Gagliani <lorenz_g@hotmail.it>
2017-02-25 15:14:59 +0100

    pullate

--- ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/login/login.component.ts
+++ ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import {FileUploaderComponent} from "../file-uploader/file-uploader.component";
 import { Cookie } from 'ng2-cookies/ng2-cookies';
 import {Router} from "@angular/router";
 
@@ -9,8 +8,7 @@ import {Router} from "@angular/router";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  providers: [FileUploaderComponent]
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
