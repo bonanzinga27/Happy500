ecd457b8242ab0c93fa0e0b39aba48e635f14c35
Lorenzo Gagliani <lorenz_g@hotmail.it>
2017-02-25 02:31:25 +0100

    form dati personali

--- ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/dati-personali/dati-personali.component.ts
+++ ebuonweekend/ebuonweekend-web/src/main/webapp/src/app/dati-personali/dati-personali.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, Validators, FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-dati-personali',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dati-personali.component.css']
 })
 export class DatiPersonaliComponent implements OnInit {
+
+  public datiPersonaliForm = new FormGroup({
+    email: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required)
+  });
 
   constructor() { }
 
